e5e0c5126bd7fd173aedd7ce756cd65c649024b5
Varsha <varsha.ragavendran@gmail.com>
2018-03-07 12:25:37 -0500

    code cleanup

--- TheIfDecisionStatement/TheIfDecisionStatement/Program.cs
+++ TheIfDecisionStatement/TheIfDecisionStatement/Program.cs
@@ -8,23 +8,25 @@ namespace TheIfDecisionStatement
         {
             Console.Write("Choose a door: 1, 2, or 3:");
             string userValue = Console.ReadLine();
+            string returnMessage;
 
             if (userValue == "1")
             {
-                Console.WriteLine("Door chosen : 1");
+                returnMessage = "Door chosen : 1";
             }
             else if (userValue == "2")
             {
-                Console.WriteLine("Door chosen : 2");
+                returnMessage = "Door chosen : 2";
             }
             else if (userValue == "3")
             {
-                Console.WriteLine("Door chosen : 3");
+                returnMessage = "Door chosen : 3";
             }
             else
             {
-                Console.WriteLine("Invalid Door chosen!");
+                returnMessage = "Invalid Door chosen!";
             }
+            Console.WriteLine(returnMessage);
         }
     }
 }

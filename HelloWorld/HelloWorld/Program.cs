4f79c3d3089cff8ce7cb797730a0b417909eb1ed
Varsha <varsha.ragavendran@gmail.com>
2018-03-07 09:43:06 -0500

    initial HelloWorld program

---
+++ HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Writes to console
+            Console.WriteLine("Hello World!");
+            Console.ReadLine();
+        }
+    }
+}

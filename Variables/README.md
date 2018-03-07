82c9c0cfddbeeb06c37908fbe57553e3a21368e1
Varsha <varsha.ragavendran@gmail.com>
2018-03-07 11:33:35 -0500

    addition of new tutorial code

---
+++ Variables/README.md
@@ -0,0 +1,3 @@
+# Data Types and Variables
+
+Tutorial going over data types, and conversion from string to int, vice versa.
---
+++ Variables/Variables/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Variables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Data Type : int 
+            int x = 5;
+            int y = x + 9;
+            Console.WriteLine(y);
+
+            // Data Type : int with input from console
+            string consoleValueOne = Console.ReadLine();
+            string consoleValueTwo = Console.ReadLine();
+            Console.WriteLine(Convert.ToInt32(consoleValueOne) + Convert.ToInt32(consoleValueTwo));
+
+            // Data Type : string 
+            Console.WriteLine("What is your Name?");
+            Console.Write("What is your First Name? : ");
+            string myFirstName = Console.ReadLine();
+            Console.Write("What is your Last Name? : ");
+            string myLastName = Console.ReadLine();
+            Console.WriteLine("Hello " + myFirstName + " " + myLastName + "!");
+        }
+    }
+}

fa9fbe64f86abcd8cca93c5164667a6b1e584ab1
Varsha <varsha.ragavendran@gmail.com>
2018-03-07 18:08:57 -0500

    addition of ReverseString method

---
+++ SimpleMethod/README.md
@@ -0,0 +1,3 @@
+# Reverse Input Strings
+
+Introduction to writing a simple method that takes in 3 inputs from the console, and reverses the characters of all 3 strings.
---
+++ SimpleMethod/SimpleMethod/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace SimpleMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter your first name: ");
+            string firstName = Console.ReadLine();
+            Console.Write("Enter your last name: ");
+            string lastName = Console.ReadLine();
+            Console.Write("Enter where you're from: ");
+            string place = Console.ReadLine();
+            DisplayResult(ReverseString(firstName), ReverseString(lastName), ReverseString(place));
+        }
+
+        /**
+         * Reverses the characters of the given string
+         */
+        private static string ReverseString(string message)
+        {
+            char[] messageArray = message.ToCharArray();
+            Array.Reverse(messageArray);
+            return String.Concat(messageArray);
+        }
+
+        /**
+         * Displays the result of reveresed strings
+         */
+        private static void DisplayResult(string reversedFirstName, string reversedLastName, string reversedPlace)
+        {
+            Console.Write("Results: ");
+            Console.Write(String.Format("{0} {1} {2}", reversedFirstName, reversedLastName, reversedPlace));
+        }
+    }
+}

36f11cdb9396c40c5169f85f6e74504ce164ef3f
Varsha <varsha.ragavendran@gmail.com>
2018-03-08 20:20:32 -0500

    code - library tutorial addition

---
+++ Lesson18/README.md
@@ -0,0 +1,2 @@
+# Client - Library Project
+A quick tutorial on creating a blank project with a code library being referenced by a client project
---
+++ Lesson18/ScrapeClient/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using ScrapeLibrary;
+namespace ScrapeClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Scrape scrape = new Scrape();
+            string value = scrape.ScrapeWebpage("http://msdn.microsoft.com");
+            Console.WriteLine(value);
+        }
+    }
+}
